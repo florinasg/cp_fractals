34a11a9d0b6a78fab171428781239277237d3900
florina <fanderl95@gmail.com>
2018-03-28 17:57:56 +0200

    Update

--- export_EIGENMODE.cpp
+++ export_EIGENMODE.cpp
@@ -18,11 +18,12 @@ int kfract::export_EIGENMODE()
 	arma::vec EIG;
 	std::ofstream eigenmode;
 
-	for(hdx = 1; hdx <= eigvec.n_cols; hdx++)
+	for(hdx = 0; hdx < eigvec.n_cols; hdx++)
 	{
 		EIG = eigvec.col(hdx);
 		eigenmode.open("Eigenmode_Level"+std::to_string(level_final)+"EM_"+std::to_string(hdx)+".csv");
-
+		grid_index = 0;
+		eigenvec_index = 0;
 
 		for(int idx = 0; idx< (grid_vector_num+1); idx++)
 		{
@@ -32,11 +33,19 @@ int kfract::export_EIGENMODE()
 				/*reads the stored grid points column-wise top-down*/
 				grid_index = (idx*(grid_vector_num+1))+jdx;
 				if(kfract_grid[grid_index][2] == 1)
+				{
 					kfract_grid[grid_index].push_back(double(EIG[eigenvec_index]));
-				else
+					eigenmode << kfract_grid.at(grid_index).at(0) << "," << kfract_grid.at(grid_index).at(1) << "," <<  kfract_grid.at(grid_index).at(2) <<","
+											<<kfract_grid.at(grid_index).at(3)<<"\n";
+				}
+				else if (kfract_grid[grid_index][2] == -1)
+				{
 					kfract_grid[grid_index].push_back(double(0));
-				eigenmode << kfract_grid.at(grid_index).at(0) << "," << kfract_grid.at(grid_index).at(1) << "," <<  kfract_grid.at(grid_index).at(2) <<","
-						<<kfract_grid.at(grid_index).at(3)<<"\n";
+					eigenmode << kfract_grid.at(grid_index).at(0) << "," << kfract_grid.at(grid_index).at(1) << "," <<  kfract_grid.at(grid_index).at(2) <<","
+											<<kfract_grid.at(grid_index).at(3)<<"\n";
+				}
+
+
 				eigenvec_index ++;
 			}
 
